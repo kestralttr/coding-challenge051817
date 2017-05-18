e96bd5e35d259c07d16b87c65df4eb8d84f1c57f
Alex Bennett <kestralttr@gmail.com>
2017-05-18 16:14:01 -0700

    successfully console logs checked items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import $ from "jquery";
 import UserList from './Component/UserList';
 import './master.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let names = [];
+    let checkedBoxes = $(".checkbox:checked").parent().parent().find(".name-section");
+    Array.prototype.forEach.call(checkedBoxes,function(el) {
+      names.push(el.innerHTML);
+    });
+    console.log(names);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>User List</h1>
         <UserList />
+        <button className="confirm-button" onClick={this.handleClick}>Confirm</button>
       </div>
     );
   }
